71a670eb35bc94081e7330a0d411b2b8e6cb6c11
zyang6 <zhouyang271@huawei>
2025-09-17 17:02:46 +0800

    测试通信代码OK

--- k8s/test/test_1.sh
+++ k8s/test/test_1.sh
@@ -1,3 +1,4 @@
+set -x
 export HCCL_SOCKET_IFNAME=ens45 # modify according to actual situation
 export TP_SOCKET_IFNAME=ens45   # modify according to actual situation
 export GLOO_SOCKET_IFNAME=ens45 # modify according to actual situation
@@ -15,11 +16,15 @@ source /etc/profile;
 
 LIB_PATH=/opt/python3.10/lib/
 export LD_LIBRARY_PATH=$LIB_PATH:$LD_LIBRARY_PATH
+
+
 export NPU_PER_NODE=8  # A2 NPU Number
 export ServerPort=6666     # modify according to actual situation
 export DashboardPort=8888  # modify according to actual situation
-
 export NNODES=$((WORLD_SIZE/NPU_PER_NODE))         # example is 4 Nodes
+unset LOCAL_WORLD_SIZE
+unset WORLD_SIZE
+unset LOCAL_RANK
 
 cd /home/new_verl
 if [ "$RANK" = "0" ]; then
@@ -28,18 +33,15 @@ if [ "$RANK" = "0" ]; then
   echo "CURRENT_IP=$CURRENT_IP"
 
   kwargs=(--is_master --ray_dashboard_port $DashboardPort)
-
 else
-  kwargs=()
-
+  kwargs=() 
 fi
+
+ray stop --force
 
 python tests/test_comm.py \
     --nnodes=${NNODES} \
     --ray_master_ip=$MASTER_ADDR \
     --ray_master_port=$ServerPort \
-    --device=npu ${kwargs[@]} 
-
-if [ "$RANK" = "0" ]; then
-    ray_name=$(ray job list | grep -o "raysubmit_[a-zA-Z0-9]*")
-fi
+    --device=npu \
+    ${kwargs[@]} 
