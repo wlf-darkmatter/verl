10919a6805f4cea0c54b2cece6f1f85ac63a5e8d
Lingfeng Wang <62014693+wlf-darkmatter@users.noreply.github.com>
2025-09-24 17:41:54 +0800

    调整检测工具执行命令

--- k8s/beijing/32nodes_nosharding/start.sh
+++ k8s/beijing/32nodes_nosharding/start.sh
@@ -17,7 +17,8 @@ rm -f /opt/vllm/vllm/model_executor/models/deepseek_v2.py
 cp -f /home/new_verl/k8s/patch/deepseek_v2.py /opt/vllm/vllm/model_executor/models/deepseek_v2.py
 #######################################
 
-mkdir -p /data01/huawei-2025/wlf/watchbash /data01/huawei-2025/wlf/verl/k8s/script/watch_stats.sh > /data01/huawei-2025/wlf/watch/rank${RANK}_${CURRENT_IP}.log &
+mkdir -p /data01/huawei-2025/wlf/watch
+bash /data01/huawei-2025/wlf/verl/k8s/script/watch_stats.sh > /data01/huawei-2025/wlf/watch/rank${RANK}_${CURRENT_IP}.log &
 
 source /usr/local/Ascend/ascend-toolkit/set_env.sh;
 source /usr/local/Ascend/nnal/atb/set_env.sh;
