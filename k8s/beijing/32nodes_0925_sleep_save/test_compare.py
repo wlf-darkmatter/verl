ecff5dcdd517548bfd672e3ae4749b452d7c9f9b
zyang6 <zhouyang271@huawei>
2025-09-25 17:21:16 +0800

    .

---
+++ k8s/beijing/32nodes_0925_sleep_save/test_compare.py
@@ -0,0 +1,4 @@
+import torch
+model_aftersharding_rank0=torch.load('/data01/huawei-2025/wlf/verl/k8s/beijing/32nodes_0925_sleep_save/data_weight/model_aftersharding_rank0.pt')
+model_nosharding_rank0=torch.load('/data01/huawei-2025/wlf/verl/k8s/beijing/32nodes_0925_sleep_save/data_weight/model_nosharding_rank0.pt')
+for 
