ecbf6d1ccb4b5af3b0f01aee80dc2f0b2cd672f7
Lingfeng Wang <62014693+wlf-darkmatter@users.noreply.github.com>
2025-10-09 17:56:26 +0800

    .

--- k8s/shanghai/new_2nodes/start.sh
+++ k8s/shanghai/new_2nodes/start.sh
@@ -26,7 +26,7 @@ export NPU_PER_NODE=8  # A2 NPU Number
 export NNODES=2         # example is 4 Nodes
 
 export path_log_dir=/opt/verl/logs/$MINDX_TASK_ID/trainlog  # modify according to actual situation
-export ASCEND_PROCESS_LOG_PATH=/home/new_verl/plog/$(basename $(dirname $0))
+export ASCEND_PROCESS_LOG_PATH=/home/new_verl/plog/$(basename $(dirname $0))/${RANK}
 
 
 ray stop --force
