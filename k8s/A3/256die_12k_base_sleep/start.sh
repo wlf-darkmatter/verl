dc56e1876cf0f60d1444777c1e24036c3f87c010
zyang6 <zhouyang271@huawei>
2025-10-12 15:50:28 +0800

    .

--- k8s/A3/256die_12k_base_sleep/start.sh
+++ k8s/A3/256die_12k_base_sleep/start.sh
@@ -11,7 +11,7 @@ export ASCEND_GLOBAL_LOG_LEVEL=3
 #! 注意，自定义配置
 export VLLM_SLEEP_LEVEL=2
 export VERL_DEBUG_NOSHARDING=0
-export VERL_MEMORY_LOG_DIR="/home/new_verl/memory/512die_12k_base_sleep2"
+export VERL_MEMORY_LOG_DIR="/home/new_verl/logs/memory/512die_12k_base_sleep2"
 export VERL_CUSTOM_REWARD_RULE="1"
 
 #! 注意，0929加了这 1 个优化参数， libjemalloc 需要重新编译
