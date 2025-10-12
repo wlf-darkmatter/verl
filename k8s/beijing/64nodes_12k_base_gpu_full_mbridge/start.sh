557ffce039b65b2f4d7df4e3bd271ab243bed84a
zyang6 <zhouyang271@huawei>
2025-10-12 18:29:35 +0800

    a2  full mbridge

--- k8s/beijing/64nodes_12k_base_gpu_full_mbridge/start.sh
+++ k8s/beijing/64nodes_12k_base_gpu_full_mbridge/start.sh
@@ -1,6 +1,6 @@
-export HCCL_SOCKET_IFNAME=bond1 # modify according to actual situation
-export TP_SOCKET_IFNAME=bond1   # modify according to actual situation
-export GLOO_SOCKET_IFNAME=bond1 # modify according to actual situation
+export HCCL_SOCKET_IFNAME=ens45 # modify according to actual situation
+export TP_SOCKET_IFNAME=ens45   # modify according to actual situation
+export GLOO_SOCKET_IFNAME=ens45 # modify according to actual situation
 # export HYDRA_FULL_ERROR=1
 export RAY_DEDUP_LOGS=1
 # export HCCL_EXEC_TIMEOUT=3600
