4daac7edc1365f0797d70401e5159d7d93c93428
zyang6 <zhouyang271@huawei.com>
2025-10-21 17:20:52 +0800

    更改北京acjob的挂载示例 beijing/64nodes_12k_base_gpu_full_profile

--- k8s/beijing/64nodes_12k_base_gpu_full_profile/hw_run_dapo_deepseek_671b_megatron.sh
+++ k8s/beijing/64nodes_12k_base_gpu_full_profile/hw_run_dapo_deepseek_671b_megatron.sh
@@ -17,17 +17,17 @@ kl_loss_coef=0.001
 clip_ratio_low=0.2
 clip_ratio_high=0.28
 max_prompt_length=$((1024 * 2))
-max_response_length=$((5))
+max_response_length=$((6))
 enable_overlong_buffer=True
 overlong_buffer_len=$((1024 * 1))
 overlong_penalty_factor=1.0
 
 loss_agg_mode="token-mean"
-train_prompt_bsz=32
+train_prompt_bsz=16
 n_resp_per_prompt=16
-train_prompt_mini_bsz=32
-train_ppo_micro_batch_size_per_gpu=2
-infer_ppo_micro_batch_size_per_gpu=2
+train_prompt_mini_bsz=16
+train_ppo_micro_batch_size_per_gpu=1 #! 1017 会议决定更改
+infer_ppo_micro_batch_size_per_gpu=1 #! 1017 会议决定更改
 # Paths
 MODEL_PATH="/data01/huawei-2025/weight/dsv3-base-hf"
 MCORE_MODEL_PATH="/data01/huawei-2025/weight/dsv3_bf16_mcore_full_base"
@@ -45,9 +45,10 @@ val_top_p=0.7
 
 # Performance Related Parameter
 use_dynamic_bsz=True
-actor_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 1))
-infer_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 3))
+actor_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 1)) #! 1017 会议决定更改
+infer_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 1)) #! 1017 会议决定更改
 
+max_num_batched_tokens=$((max_prompt_length + max_response_length))
 optimizer_offload_fraction=1
 
 
@@ -77,16 +78,28 @@ ETP=1
 #    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_router_dtype=fp32 \
 #   +actor_rollout_ref.actor.megatron.override_transformer_config.moe_grouped_gemm=True \
 #   +actor_rollout_ref.actor.megatron.override_transformer_config.moe_token_dispatcher_type="alltoall" \
+#
 RUNTIME_ENV=verl/trainer/mc2_env.yaml
 cd /opt/verl
 ray job submit --runtime-env="${RUNTIME_ENV}" \
     -- python3 -m recipe.dapo.main_dapo \
     --config-path=config \
     --config-name="dapo_megatron_trainer" \
-    actor_rollout_ref.rollout.load_format=safetensors \
-    actor_rollout_ref.rollout.skip.enable=True \
-    actor_rollout_ref.rollout.skip.dump_dir="/data01/huawei-2025/wlf/rollout_dump/baseline_gpu_gbs32_1016" \
+    actor_rollout_ref.rollout.load_format=dummy \
+    actor_rollout_ref.rollout.skip.enable=False \
+    actor_rollout_ref.rollout.skip.dump_dir=${JOB_LOG_DIR}/rollout_skip \
     actor_rollout_ref.rollout.skip.max_dump_step=500 \
+    actor_rollout_ref.rollout.profiler.enable=True \
+    actor_rollout_ref.ref.profiler.enable=False \
+    actor_rollout_ref.actor.profiler.enable=True \
+    actor_rollout_ref.actor.profiler.ranks="[0,1,2,3,4,5,6,7]" \
+    actor_rollout_ref.actor.profiler.tool_config.npu.level=level1 \
+    actor_rollout_ref.actor.profiler.tool_config.npu.analysis=True \
+    actor_rollout_ref.actor.profiler.tool_config.npu.discrete=False \
+    actor_rollout_ref.actor.profiler.tool_config.npu.contents="[cpu,npu,memory,module]" \
+    global_profiler.save_path=${JOB_LOG_DIR_CURR}/profile \
+    global_profiler.steps="[1,2]" \
+    global_profiler.tool="npu" \
     data.train_files="${TRAIN_FILE}" \
     data.val_files="${TEST_FILE}" \
     data.prompt_key=messages \
@@ -99,7 +112,7 @@ ray job submit --runtime-env="${RUNTIME_ENV}" \
     algorithm.use_kl_in_reward=${use_kl_in_reward} \
     algorithm.kl_penalty=${kl_penalty} \
     algorithm.kl_ctrl.kl_coef=${kl_coef} \
-    actor_rollout_ref.model.path="${MODEL_PATH}" \
+    actor_rollout_ref.model.path=${MODEL_PATH} \
     actor_rollout_ref.actor.use_kl_loss=${use_kl_loss} \
     actor_rollout_ref.actor.kl_loss_coef=${kl_loss_coef} \
     actor_rollout_ref.actor.policy_loss.loss_mode=vanilla \
@@ -116,14 +129,10 @@ ray job submit --runtime-env="${RUNTIME_ENV}" \
     +actor_rollout_ref.actor.megatron.override_transformer_config.num_layers_in_first_pipeline_stage=$first_layer \
     +actor_rollout_ref.actor.megatron.override_transformer_config.num_layers_in_last_pipeline_stage=$last_layer \
     +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_offload_fraction=${optimizer_offload_fraction} \
-    +actor_rollout_ref.actor.optim.override_optimizer_config.overlap_cpu_optimizer_d2h_h2d=True \
-    +actor_rollout_ref.actor.optim.override_optimizer_config.use_precision_aware_optimizer=True \
     +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_cpu_offload=True \
-    actor_rollout_ref.actor.megatron.param_offload=True \
-    actor_rollout_ref.actor.megatron.optimizer_offload=True \
-    actor_rollout_ref.actor.megatron.grad_offload=True \
-    actor_rollout_ref.ref.megatron.param_offload=True \
-    ++actor_rollout_ref.actor.megatron.override_transformer_config.attention_backend=fused \
+    actor_rollout_ref.actor.megatron.param_offload=${offload} \
+    actor_rollout_ref.actor.megatron.optimizer_offload=${offload} \
+    actor_rollout_ref.actor.megatron.grad_offload=${offload} \
     actor_rollout_ref.actor.megatron.use_mbridge=$USE_MBRIDGE \
     actor_rollout_ref.actor.megatron.use_dist_checkpointing=$USE_DIST_CKPT \
     actor_rollout_ref.actor.megatron.tensor_model_parallel_size=${train_tp} \
@@ -134,23 +143,20 @@ ray job submit --runtime-env="${RUNTIME_ENV}" \
     +actor_rollout_ref.actor.megatron.override_transformer_config.use_flash_attn=True \
     actor_rollout_ref.actor.megatron.expert_tensor_parallel_size=$ETP \
     actor_rollout_ref.ref.megatron.expert_tensor_parallel_size=$ETP \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.bias_dropout_fusion=True \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.persist_layer_norm=True \
     +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_method=uniform \
     +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_granularity=full \
     +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_num_layers=1 \
-    +actor_rollout_ref.actor.profiler.tool="torch_memory" \
-    actor_rollout_ref.actor.profiler.save_path=/data01/huawei-2025/outputs/snapshot\
+    ++actor_rollout_ref.actor.megatron.override_transformer_config.attention_backend=fused \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.actor.loss_agg_mode=${loss_agg_mode} \
-    actor_rollout_ref.rollout.load_format=safetensors \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${infer_ppo_micro_batch_size_per_gpu} \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.65 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=${gen_tp} \
     actor_rollout_ref.rollout.dp_model_parallel_size=${gen_dp} \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
-    actor_rollout_ref.rollout.max_num_batched_tokens=$((max_prompt_length + max_response_length)) \
+    actor_rollout_ref.rollout.enable_prefix_caching=False \
+    actor_rollout_ref.rollout.max_num_batched_tokens=${max_num_batched_tokens} \
     actor_rollout_ref.rollout.temperature=${temperature} \
     actor_rollout_ref.rollout.top_p=${top_p} \
     actor_rollout_ref.rollout.top_k=${top_k} \
@@ -181,21 +187,22 @@ ray job submit --runtime-env="${RUNTIME_ENV}" \
     trainer.project_name="${project_name}" \
     trainer.experiment_name="${exp_name}" \
     trainer.n_gpus_per_node="${NPU_PER_NODE}" \
+    trainer.balance_batch=False \
     trainer.nnodes="${NNODES}" \
     trainer.val_before_train=False \
     trainer.test_freq=-1 \
     trainer.save_freq=10 \
     trainer.total_epochs=10 \
-    trainer.balance_batch=False \
     trainer.default_local_dir=${CKPTS_DIR} \
     trainer.resume_mode=auto \
-    trainer.rollout_data_dir=/data01/huawei-2025/lq/${exp_name}/1/rollout \
+    trainer.rollout_data_dir=${JOB_LOG_DIR_CURR}/rollout_data_dir \
     trainer.log_val_generations=10 \
-    trainer.device="npu" $@ 2>&1 | tee /tmp/ray.output
+    trainer.device="npu" $@ 2>&1
 
-
-sleep 600
-ray_name=$(cat /tmp/ray.output | grep "submitted successfully" | awk -F "'" '{print $2}')
-ray_name=${ray_name//\'}
-echo "ray_name: $ray_name"
-ray job logs $ray_name --follow | tee $(dirname $0)/ray.log
+#! 以下优化特性被舍弃
+# 被取消的参数
+#    +actor_rollout_ref.actor.megatron.override_transformer_config.bias_dropout_fusion=True \
+#    +actor_rollout_ref.actor.megatron.override_transformer_config.persist_layer_norm=True \
+#     +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_cpu_offload=True \
+#! 以下特性不使用会导致报错！
+#     ++actor_rollout_ref.actor.megatron.override_transformer_config.attention_backend=fused \
