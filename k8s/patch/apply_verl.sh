70f25a25841e8a9c185489e788690e3e859b542a
zyang6 <zhouyang271@huawei.com>
2025-10-21 09:57:16 +0800

    添加CP的patch

--- k8s/patch/apply_verl.sh
+++ k8s/patch/apply_verl.sh
@@ -1,6 +1,5 @@
 if [[ ${USE_CP_PATCH} == 1 ]];then
     #! 需要使用新的mindspeed
-    unalias cp
     cd /opt/verl
     git apply /home/code/verl/k8s/patch/verl.patch/verl0928-cp/mcore_util.diff
     git apply /home/code/verl/k8s/patch/verl.patch/verl0928-cp/model_forward.diff
