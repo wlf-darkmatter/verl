e690cefed8221a0b65edff4026ed40eaa6d10155
zyang6 <zhouyang271@huawei.com>
2025-10-22 09:49:54 +0800

    添加megatron的mtp部分的patch

--- k8s/patch/apply_megatron.sh
+++ k8s/patch/apply_megatron.sh
@@ -6,6 +6,11 @@ if [[ ${megatron_version} == 12 ]];then
 
     rm -f /opt/Megatron-LM/megatron/core/transformer/dot_product_attention.py
     cp -f /home/code/verl/k8s/patch/megatron.patch/0.12.1/Megatron-LM/megatron/core/transformer/dot_product_attention.py /opt/Megatron-LM/megatron/core/transformer/dot_product_attention.py
+
+    rm -f /opt/Megatron-LM/megatron/core/transformer/multi_token_prediction.py
+    cp -f /home/code/verl/k8s/patch/megatron.patch/0.12.1/Megatron-LM/megatron/core/transformer/multi_token_prediction.py /opt/Megatron-LM/megatron/core/transformer/multi_token_prediction.py
+
     echo -e "\033[32mApplied Megatron-core ${megatron_version}!\033[0m"
 
 fi
+
