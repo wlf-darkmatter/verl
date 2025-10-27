e3d8677f9013c32443203959783fa190484da1df
Lingfeng Wang <62014693+wlf-darkmatter@users.noreply.github.com>
2025-10-27 11:18:40 +0800

    .

--- k8s/beijing/64nodes_12k-base-gpunpu-1027/gpu_1004-1009.sh
+++ k8s/beijing/64nodes_12k-base-gpunpu-1027/gpu_1004-1009.sh
@@ -2,8 +2,12 @@ set -x
 
 echo ">>Starting script at: $(date), path = $(pwd)"
 
-project_name='DAPO'
-exp_name='DAPO-dpsk-671b-megatron-BASE-16NNODES-0827bimages'
+NNODES=${NNODES:-1}
+export NNODES=16
+NGPUS_PER_NODES=${NGPUS_PER_NODES:-8}
+cd /afs/chatrl/users/lyy/npu/verl
+project_name='dpsk_v3_base_zero'
+exp_name='dist_pp8_ep16_tp8_offload1_2k_12k_flex'
 
 adv_estimator=grpo
 
@@ -26,14 +30,15 @@ loss_agg_mode="token-mean"
 train_prompt_bsz=32
 n_resp_per_prompt=16
 train_prompt_mini_bsz=32
-train_ppo_micro_batch_size_per_gpu=2 #! 1027 现场决定更改
-infer_ppo_micro_batch_size_per_gpu=2 #! 1027 现场决定更改
+train_ppo_micro_batch_size_per_gpu=2
+infer_ppo_micro_batch_size_per_gpu=2
 # Paths
-MODEL_PATH="/data01/huawei-2025/weight/dsv3-base-hf"
-MCORE_MODEL_PATH="/data01/huawei-2025/weight/dsv3_bf16_mcore_full_base"
-#CKPTS_DIR=/data01/huawei-2025/weight/ckpt-DAPO-DeepSeek-671b-megatron-base-2k12k-gpu-vllm010
-TRAIN_FILE="/data01/huawei-2025/rl_data/dapo-math/dapo-math-17k_dedup_r1_sys_prompt_mathdapo.parquet"
-TEST_FILE="/data01/huawei-2025/rl_data/dapo-math/dapo-math-17k_dedup_r1_sys_prompt_mathdapo.parquet"
+MODEL_PATH=/afs/chatrl/public/models/DeepSeek-V3-Base-bf16
+DIST_CKPT_PATH=/afs/chatrl/public/models/DeepSeek-V3-Base-bf16-dist
+
+RAY_DATA_HOME=${RAY_DATA_HOME:-"${HOME}/verl"}
+TRAIN_FILE=/afs/chatrl/users/lyy/npu/data/dapo-math-17k_dedup_r1_sys_prompt_mathdapo.parquet
+TEST_FILE=/afs/chatrl/users/lyy/npu/data/aime24.parquet
 # TEST_FILE="['$aime24_test_path']"
 
 # Algorithm
@@ -44,46 +49,64 @@ val_top_p=0.7
 
 # Performance Related Parameter
 use_dynamic_bsz=True
-actor_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 1)) #! 1027 现场决定更改
-infer_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 3)) #! 1027 现场决定更改
+actor_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 1))
+infer_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 3))
 
-max_num_batched_tokens=$((max_prompt_length + max_response_length))
 optimizer_offload_fraction=1
 
+COMMON_PP=${COMMON_PP:-16}
+COMMON_VPP=${COMMON_VPP:-null}
+COMMON_CP=${COMMON_CP:-1}
+COMMON_TP=${COMMON_TP:-8}
+COMMON_EP=${COMMON_EP:-8}
+COMMON_ETP=${COMMON_ETP:-1}
+
+TRAIN_TP=${TRAIN_TP:-$COMMON_TP}
+INFER_TP=${INFER_TP:-32}
+
+ACTOR_PP=${ACTOR_PP:-$COMMON_PP}
+ACTOR_VPP=${ACTOR_VPP:-$COMMON_VPP}
+ACTOR_CP=${ACTOR_CP:-$COMMON_CP}
+ACTOR_TP=${ACTOR_TP:-$TRAIN_TP}
+ACTOR_EP=${ACTOR_EP:-$COMMON_EP}
+ACTOR_ETP=${ACTOR_ETP:-$COMMON_ETP}
+ROLLOUT_TP=${ROLLOUT_TP:-$INFER_TP}
+REF_PP=${REF_PP:-$COMMON_PP}
+REF_VPP=${REF_VPP:-$COMMON_VPP}
+REF_CP=${REF_CP:-$COMMON_CP}
+REF_TP=${REF_TP:-$TRAIN_TP}
+REF_EP=${REF_EP:-$COMMON_EP}
+REF_ETP=${REF_ETP:-$COMMON_ETP}
+CRITIC_PP=${CRITIC_PP:-$COMMON_PP}
+CRITIC_VPP=${CRITIC_VPP:-$COMMON_VPP}
+CRITIC_CP=${CRITIC_CP:-$COMMON_CP}
+CRITIC_TP=${CRITIC_TP:-$TRAIN_TP}
+CRITIC_EP=${CRITIC_EP:-$COMMON_EP}
+CRITIC_ETP=${CRITIC_ETP:-$COMMON_ETP}
+RM_PP=${RM_PP:-$COMMON_PP}
+RM_VPP=${RM_VPP:-$COMMON_VPP}
+RM_CP=${RM_CP:-$COMMON_CP}
+RM_TP=${RM_TP:-$TRAIN_TP}
+RM_EP=${RM_EP:-$COMMON_EP}
+RM_ETP=${RM_ETP:-$COMMON_ETP}
 
 # install mbridge
 # pip3 install git+https://github.com/ISEEKYAN/mbridge
-USE_MBRIDGE=False
-USE_DIST_CKPT=True
+USE_MBRIDGE=True
+USE_DIST_CKPT=False
 
 
 # first_layer=6
 # last_layer=7
 # pipeline_num_transformer_layers="[[6],[8],[8],[8],[8],[8],[8],[7]]"
-first_layer=6
-last_layer=7
+first_layer=3
+last_layer=2
 # pipeline_num_transformer_layers="[[3],[4],[4],[4],[4],[4],[4],[4],[4],[4],[4],[4],[4],[4],[4],[2]]"
-offload=True
-gen_tp=16
-gen_dp=4
 
-train_tp=8
-train_ep=64
-train_pp=8
-enable_filter_group=False
-train_cp=1
-#    +actor_rollout_ref.actor.megatron.override_transformer_config.context_parallel_size=${train_cp} \
-ETP=1
-#    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_router_dtype=fp32 \
-#   +actor_rollout_ref.actor.megatron.override_transformer_config.moe_grouped_gemm=True \
-#   +actor_rollout_ref.actor.megatron.override_transformer_config.moe_token_dispatcher_type="alltoall" \
-#
-RUNTIME_ENV=verl/trainer/mc2_env.yaml
-cd /opt/verl
-ray job submit --runtime-env="${RUNTIME_ENV}" \
-    -- python3 -m recipe.dapo.main_dapo \
+python3 -m recipe.dapo.main_dapo \
     --config-path=config \
     --config-name="dapo_megatron_trainer" \
+    actor_rollout_ref.nccl_timeout=7200 \
     data.train_files="${TRAIN_FILE}" \
     data.val_files="${TEST_FILE}" \
     data.prompt_key=messages \
@@ -96,7 +119,7 @@ ray job submit --runtime-env="${RUNTIME_ENV}" \
     algorithm.use_kl_in_reward=${use_kl_in_reward} \
     algorithm.kl_penalty=${kl_penalty} \
     algorithm.kl_ctrl.kl_coef=${kl_coef} \
-    actor_rollout_ref.model.path=${MODEL_PATH} \
+    actor_rollout_ref.model.path="${MODEL_PATH}" \
     actor_rollout_ref.actor.use_kl_loss=${use_kl_loss} \
     actor_rollout_ref.actor.kl_loss_coef=${kl_loss_coef} \
     actor_rollout_ref.actor.policy_loss.loss_mode=vanilla \
@@ -113,38 +136,48 @@ ray job submit --runtime-env="${RUNTIME_ENV}" \
     +actor_rollout_ref.actor.megatron.override_transformer_config.num_layers_in_first_pipeline_stage=$first_layer \
     +actor_rollout_ref.actor.megatron.override_transformer_config.num_layers_in_last_pipeline_stage=$last_layer \
     +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_offload_fraction=${optimizer_offload_fraction} \
-    +actor_rollout_ref.actor.optim.override_optimizer_config.overlap_cpu_optimizer_d2h_h2d=False \
-    +actor_rollout_ref.actor.optim.override_optimizer_config.use_precision_aware_optimizer=False \
+    +actor_rollout_ref.actor.optim.override_optimizer_config.overlap_cpu_optimizer_d2h_h2d=True \
+    +actor_rollout_ref.actor.optim.override_optimizer_config.use_precision_aware_optimizer=True \
     +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_cpu_offload=True \
-    actor_rollout_ref.actor.megatron.param_offload=${offload} \
-    actor_rollout_ref.actor.megatron.optimizer_offload=${offload} \
-    actor_rollout_ref.actor.megatron.grad_offload=${offload} \
-    actor_rollout_ref.ref.megatron.param_offload=${offload} \
+    actor_rollout_ref.actor.megatron.param_offload=True \
+    actor_rollout_ref.actor.megatron.optimizer_offload=True \
+    actor_rollout_ref.actor.megatron.grad_offload=True \
+    actor_rollout_ref.ref.megatron.param_offload=True \
     ++actor_rollout_ref.actor.megatron.override_transformer_config.attention_backend=fused \
     actor_rollout_ref.actor.megatron.use_mbridge=$USE_MBRIDGE \
     actor_rollout_ref.actor.megatron.use_dist_checkpointing=$USE_DIST_CKPT \
-    actor_rollout_ref.actor.megatron.dist_checkpointing_path=${MCORE_MODEL_PATH} \
-    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=${train_pp} \
-    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=${train_tp} \
-    actor_rollout_ref.actor.megatron.expert_model_parallel_size=${train_ep} \
-    +actor_rollout_ref.ref.megatron.override_transformer_config.use_flash_attn=True \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.use_flash_attn=True \
-    actor_rollout_ref.actor.megatron.expert_tensor_parallel_size=$ETP \
-    actor_rollout_ref.ref.megatron.expert_tensor_parallel_size=$ETP \
+    actor_rollout_ref.actor.megatron.dist_checkpointing_path=${DIST_CKPT_PATH} \
+    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=${ACTOR_TP} \
+    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=${ACTOR_PP} \
+    actor_rollout_ref.actor.megatron.virtual_pipeline_model_parallel_size=${ACTOR_VPP} \
+    actor_rollout_ref.actor.megatron.context_parallel_size=${ACTOR_CP} \
+    actor_rollout_ref.actor.megatron.expert_model_parallel_size=${ACTOR_EP} \
+    actor_rollout_ref.actor.megatron.expert_tensor_parallel_size=${ACTOR_ETP} \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.apply_rope_fusion=False \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.masked_softmax_fusion=True \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.bias_activation_fusion=True \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.bias_dropout_fusion=True \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.gradient_accumulation_fusion=True \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.deallocate_pipeline_outputs=True \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.persist_layer_norm=True \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_grouped_gemm=True \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_permute_fusion=True \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_shared_expert_overlap=True \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_token_dispatcher_type="alltoall" \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_router_dtype=fp32 \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_enable_deepep=False \
     +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_method=uniform \
     +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_granularity=full \
     +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_num_layers=1 \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.actor.loss_agg_mode=${loss_agg_mode} \
-    actor_rollout_ref.rollout.load_format=safetensors \
+    actor_rollout_ref.rollout.load_format=dummy \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${infer_ppo_micro_batch_size_per_gpu} \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.65 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=${gen_tp} \
-    actor_rollout_ref.rollout.dp_model_parallel_size=${gen_dp} \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=${INFER_TP} \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
-    actor_rollout_ref.rollout.enable_prefix_caching=False \
-    actor_rollout_ref.rollout.max_num_batched_tokens=${max_num_batched_tokens} \
+    actor_rollout_ref.rollout.max_num_batched_tokens=$((max_prompt_length + max_response_length)) \
     actor_rollout_ref.rollout.temperature=${temperature} \
     actor_rollout_ref.rollout.top_p=${top_p} \
     actor_rollout_ref.rollout.top_k=${top_k} \
@@ -158,38 +191,31 @@ ray job submit --runtime-env="${RUNTIME_ENV}" \
     actor_rollout_ref.rollout.free_cache_engine=True \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=${infer_ppo_micro_batch_size_per_gpu} \
     actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
-    actor_rollout_ref.ref.megatron.dist_checkpointing_path=${MCORE_MODEL_PATH} \
     actor_rollout_ref.ref.megatron.use_dist_checkpointing=${USE_DIST_CKPT} \
-    actor_rollout_ref.ref.megatron.tensor_model_parallel_size=${train_tp} \
-    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=${train_pp} \
-    actor_rollout_ref.ref.megatron.expert_model_parallel_size=${train_ep} \
-    actor_rollout_ref.ref.megatron.expert_tensor_parallel_size=${ETP} \
+    actor_rollout_ref.ref.megatron.param_offload=${offload} \
+    actor_rollout_ref.ref.megatron.tensor_model_parallel_size=${REF_TP} \
+    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=${REF_PP} \
+    actor_rollout_ref.ref.megatron.virtual_pipeline_model_parallel_size=${REF_VPP} \
+    actor_rollout_ref.ref.megatron.context_parallel_size=${REF_CP} \
+    actor_rollout_ref.ref.megatron.expert_model_parallel_size=${REF_EP} \
+    actor_rollout_ref.ref.megatron.expert_tensor_parallel_size=${REF_ETP} \
     reward_model.reward_manager=dapo \
     +reward_model.reward_kwargs.overlong_buffer_cfg.enable=${enable_overlong_buffer} \
     +reward_model.reward_kwargs.overlong_buffer_cfg.len=${overlong_buffer_len} \
     +reward_model.reward_kwargs.overlong_buffer_cfg.penalty_factor=${overlong_penalty_factor} \
     +reward_model.reward_kwargs.overlong_buffer_cfg.log=False \
     +reward_model.reward_kwargs.max_resp_len=${max_response_length} \
-    trainer.logger=['console'] \
+    trainer.logger=['tensorboard'] \
     trainer.project_name="${project_name}" \
     trainer.experiment_name="${exp_name}" \
-    trainer.n_gpus_per_node="${NPU_PER_NODE}" \
-    trainer.balance_batch=False \
+    trainer.n_gpus_per_node="${NGPUS_PER_NODES}" \
     trainer.nnodes="${NNODES}" \
     trainer.val_before_train=False \
+    trainer.balance_batch=False \
     trainer.test_freq=-1 \
-    trainer.save_freq=10 \
+    trainer.save_freq=-1 \
     trainer.total_epochs=10 \
-    trainer.default_local_dir=${CKPTS_DIR} \
+    trainer.default_local_dir=$EXP_DIR \
     trainer.resume_mode=auto \
-    trainer.rollout_data_dir=/data01/huawei-2025/lq/$(basename $(dirname $0))/rollout \
-    trainer.log_val_generations=10 \
-    trainer.device="npu" $@ 2>&1 | tee /tmp/ray.output
-
-#! 以下优化特性被舍弃
-# 被取消的参数
-#    +actor_rollout_ref.actor.megatron.override_transformer_config.bias_dropout_fusion=True \
-#    +actor_rollout_ref.actor.megatron.override_transformer_config.persist_layer_norm=True \
-#     +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_cpu_offload=True \
-#     ++actor_rollout_ref.actor.megatron.override_transformer_config.attention_backend=fused \
-#    ++actor_rollout_ref.actor.megatron.override_transformer_config.attention_backend=fused \
+    trainer.rollout_data_dir=$EXP_DIR/rollout \
+    trainer.log_val_generations=10 2>&1 | tee $EXP_DIR/run_$(date +%Y%m%d%H%M%S).log
---
+++ k8s/beijing/64nodes_12k-base-gpunpu-1027/start.sh
@@ -0,0 +1,182 @@
+export HCCL_SOCKET_IFNAME=ens45 # modify according to actual situation
+export TP_SOCKET_IFNAME=ens45   # modify according to actual situation
+export GLOO_SOCKET_IFNAME=ens45 # modify according to actual situation
+# export HYDRA_FULL_ERROR=1
+export RAY_DEDUP_LOGS=0
+
+
+
+export PYTORCH_NPU_ALLOC_CONF="max_split_size_mb:2048"
+
+
+
+export ASCEND_GLOBAL_LOG_LEVEL=3
+
+#! 注意，0929加了这 1 个优化参数， libjemalloc 需要重新编译
+# export LD_PRELOAD="/usr/local/lib/libjemalloc.so.2"
+export TASK_QUEUE_ENABLE=2
+#! 注意，HCCL 相关配置
+export HCCL_EXEC_TIMEOUT=86400
+export HCCL_EVENT_TIMEOUT=86400
+export ACL_DEVICE_SYNC_TIMEOUT=86400
+export HCCL_ASYNC_ERROR_HANDLING=0
+export P2P_HCCL_BUFFSIZE=20
+export HCCL_BUFFSIZE=300
+export VLLM_ASCEND_ENABLE_MOE_ALL2ALL_SEQ=1
+
+
+CURRENT_IP=$(ifconfig $TP_SOCKET_IFNAME | grep -Eo 'inet (addr:)?([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print $NF}')
+
+#######################################
+#! 规避模型加载时 权重读取错误的问题
+#! [VLLM]
+#* 规避直接读 hf 权重的报错（出现减层或者带有MTP）
+rm -f /opt/vllm/vllm/model_executor/models/deepseek_v2.py
+cp -f /home/code/verl/k8s/patch/0928/vllm/vllm/model_executor/models/deepseek_v2.py /opt/vllm/vllm/model_executor/models/deepseek_v2.py
+
+
+#! [VLLM-ASCEND]
+
+rm -f /opt/vllm-ascend/vllm_ascend/models/deepseek_v2.py
+cp -f /home/code/verl/k8s/patch/0928/vllm-ascend/vllm_ascend/models/deepseek_v2.py /opt/vllm-ascend/vllm_ascend/models/deepseek_v2.py
+
+
+#! [Megatron]
+rm -f /opt/Megatron-LM/megatron/core/transformer/dot_product_attention.py
+cp -f /home/code/verl/k8s/patch/0928/Megatron-LM/megatron/dot_product_attention.py /opt/Megatron-LM/megatron/core/transformer/dot_product_attention.py
+
+
+
+#######################################
+
+source /usr/local/Ascend/ascend-toolkit/set_env.sh;
+source /usr/local/Ascend/nnal/atb/set_env.sh;
+source /opt/pyvenv/bin/activate;
+
+
+
+LIB_PATH=/opt/python3.10/lib/
+export LD_LIBRARY_PATH=$LIB_PATH:$LD_LIBRARY_PATH
+
+unset LOCAL_WORLD_SIZE
+# unset WORLD_SIZE
+unset LOCAL_RANK
+
+# export ASCEND_GLOBAL_LOG_LEVEL=1
+# export ASCEND_LAUNCH_BLOCKING=1
+
+export NPU_PER_NODE=8  # A2 NPU Number
+export NNODES=64         # example is 4 Nodes
+
+export path_log_dir=/opt/verl/logs/$MINDX_TASK_ID/trainlog  # modify according to actual situation
+export ASCEND_PROCESS_LOG_PATH=/home/code/verl/plog/$(basename $(dirname $0))/${RANK}
+
+
+ray stop --force
+sleep 10
+rm -rf /tmp/ray
+rm -rf /opt/verl
+cp -r /home/code/verl /opt/verl
+cd $(dirname $0)
+
+export ServerPort=6666     # modify according to actual situation
+export DashboardPort=8888  # modify according to actual situation
+
+cnt=0
+if [ "$RANK" = "0" ]; then
+  # head start
+  echo "This is head node"
+  mkdir -p ${JOB_LOG_DIR_CURR}
+  mkdir -p ${JOB_LOG_DIR_CURR}/ray_host
+  echo "CURRENT_IP=$CURRENT_IP"
+#   ln -s ${JOB_LOG_DIR_CURR}/ray_host /tmp/ray
+  #* 拷贝当前脚本文件
+  mkdir -p ${JOB_LOG_DIR_CURR}/script.bak
+  cp $(dirname $0)/*.sh ${JOB_LOG_DIR_CURR}/script.bak/
+  cp $(dirname $0)/*.yaml ${JOB_LOG_DIR_CURR}/script.bak/
+
+  ray start --head --port $ServerPort --dashboard-port=$DashboardPort --node-ip-address=$CURRENT_IP --dashboard-host=$CURRENT_IP --disable-usage-stats
+
+  while [[ $cnt -lt 100 ]]; do
+    ray_status_output=$(ray status)
+    npu_count=$(echo "$ray_status_output" | grep -oP '(?<=/)\d+\.\d+(?=\s*NPU)' | head -n 1)
+    npu_count_int=$(echo "$npu_count" | awk '{print int($1)}')
+
+    # judge npu_count_int bigger than NNODES*NPU_PER_NODE
+    if [ "$npu_count_int" -ge "$((NNODES*NPU_PER_NODE))" ]; then
+      echo "Ray cluster is ready with $npu_count_int npu (from $npu_count NPU resources), starting Python script."
+      bash hw_run_dapo_deepseek_671b_megatron.sh | tee ${JOB_LOG_DIR_CURR}/ray_host/$(date +"%Y-%m-%d_%H-%M-%S")_ray.log
+      break
+    fi
+
+    echo "Waiting for Ray to allocate $((NNODES*NPU_PER_NODE)) devices. Current device count: $npu_count_int"
+    cnt=$((cnt+1))
+
+  done
+
+else
+  echo "This is worker node"
+  sleep 10
+  ray start --address="$MASTER_ADDR:$ServerPort" --disable-usage-stats
+fi
+
+# start Mark 1
+
+cnt=0
+while true; do
+  ray_name=$(ray job list | grep -o "raysubmit_[a-zA-Z0-9]*")
+  if [[ -n $ray_name ]]; then
+    echo "Job $ray_name start succeeded"
+    break
+  fi
+
+  cnt=$((cnt+1))
+  if [[ $cnt -gt 100 ]]; then
+    echo "Job $ray_name start failed"
+    ray stop --force
+    sleep 10
+
+    rm -rf /tmp
+    exit 1
+  fi
+
+  sleep 50
+done
+
+ray_name=$(ray job list | grep -o "raysubmit_[a-zA-Z0-9]*")
+while true; do
+  output=$(ray job status $ray_name)
+  failed=$(echo $output | grep $ray_name | grep -i failed)
+  succeeded=$(echo $output | grep $ray_name | grep -i succeeded)
+  gcs_error=$(echo $output | grep -i 'Failed to get cluster ID from GCS server')
+
+  if [[ -n $gcs_error ]]; then
+    echo "ray cannot connect，Job $ray_name exit with exception"
+    ray stop --force
+    sleep 10
+
+   # rm -rf /tmp
+    exit 1
+  fi
+
+
+  if [[ -n $succeeded ]]; then
+    ray stop --force
+    sleep 10
+
+ #   rm -rf /tmp
+    echo "Job $ray_name exit without exception"
+    exit 0
+  fi
+
+#   if [[ -n $failed ]]; then
+#     echo "Job $ray_name exit with exception"
+#     ray stop --force
+    sleep 10
+
+# #    rm -rf /tmp
+#     exit 1
+#   fi
+
+  sleep 10
+done
