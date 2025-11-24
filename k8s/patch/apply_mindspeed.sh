9d2ab80152bf537188fa47be103ca13794ac6fe1
Lingfeng Wang <62014693+wlf-darkmatter@users.noreply.github.com>
2025-11-24 19:32:09 +0800

    适配CP，需要在start.sh里添加 USE_CP_PATCH=1

--- k8s/patch/apply_mindspeed.sh
+++ k8s/patch/apply_mindspeed.sh
@@ -14,7 +14,7 @@ if [[ $(git branch -a | grep -c 2.2.0_core_r0.12.1) -eq 0 ]];then
         cp -r ../../tmp/MindSpeed /opt/MindSpeed
     else
         echo -e "\033[1;31mMindSpeed not found, please clone it from gitcode. 然后记得放在 <大verl>/tmp/MindSpeed 里面 \033[0m"
-        # exit 1
+        exit 1
     fi
 fi
 
@@ -22,12 +22,31 @@ cd /opt/MindSpeed
 git reset --hard origin/2.2.0_core_r0.12.1
 
 cd $cwd
-\cp ./mindspeed.patch/2.2.0_core_r0.12.1/MindSpeed/mindspeed/te/pytorch/module/grouped_linear.py /opt/MindSpeed/mindspeed/te/pytorch/module/grouped_linear.py
 
-echo -e "\033[1;32mApplied MindSpeed CP\033[0m"
+cd /opt/MindSpeed
+git apply $cwd/mindspeed.patch/2.2.0_core_r0.12.1/grouped_linear.diff
 
-\cp ./mindspeed.patch/2.2.0_core_r0.12.1/MindSpeed/mindspeed/core/transformer/flash_attention/flash_attention/adaptor.py /opt/MindSpeed/mindspeed/core/transformer/flash_attention/flash_attention/adaptor.py
-
+\cp $cwd/mindspeed.patch/2.2.0_core_r0.12.1/MindSpeed/mindspeed/core/transformer/flash_attention/flash_attention/adaptor.py /opt/MindSpeed/mindspeed/core/transformer/flash_attention/flash_attention/adaptor.py
 echo -e "\033[1;32mApplied MindSpeed Attention Scale\033[0m"
+
+if [[ ${USE_CP_PATCH} == 1 ]];then
+    set -e
+    cd /opt/MindSpeed
+    git apply $cwd/mindspeed.patch/2.2.0_core_r0.12.1-cp/reset_attention_mask_adaptor.diff
+    git apply $cwd/mindspeed.patch/2.2.0_core_r0.12.1-cp/reset_attention_mask_feature.diff
+    git apply $cwd/mindspeed.patch/2.2.0_core_r0.12.1-cp/dot_product_attention.diff
+    echo -e "\033[1;33mApplied MindSpeed CP \033[0m"
+    set +e
+
+
+    cd /opt/verl
+    git apply $cwd/verl.patch/patch_v012.diff
+    git apply $cwd/verl.patch/model_forward.diff
+    git apply $cwd/verl.patch/mcore_util.diff
+
+    echo -e "\033[1;33mApplied Verl CP \033[0m"
+
+fi
+
 
 set +x # 关闭调试模式
